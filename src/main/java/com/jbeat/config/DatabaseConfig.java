fa42a82dcb599b8a01b5755d876d3d29006a5fcd
minhg <minhg337@gmail.com>
2026-06-08 22:39:31 +0900

    DataBaseConfig수정

--- src/main/java/com/jbeat/config/DatabaseConfig.java
+++ src/main/java/com/jbeat/config/DatabaseConfig.java
@@ -5,17 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    static String getEnvOrDefault(String key, String defaultValue) {
+    private static String getEnvOrDefault(String key, String defaultValue) {
         String value = System.getenv(key);
         return (value != null) ? value : defaultValue;
     }
 
-
-    static String url = getEnvOrDefault("DB_URL", "jdbc:mysql://localhost:3306/jbeat?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8");
-    static String user = getEnvOrDefault("DB_USER", "root");
-    static String password = getEnvOrDefault("DB_PASS", "mysql");
-
     public static Connection getConnection() throws SQLException {
+        try {
+            // PostgreSQL 드라이버 명시적 로드
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("PostgreSQL 드라이버를 찾을 수 없습니다!", e);
+        }
+
+        String url = getEnvOrDefault("DB_URL", "jdbc:mysql://localhost:3306/jbeat?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8");
+        String user = getEnvOrDefault("DB_USER", "root");
+        String password = getEnvOrDefault("DB_PASS", "mysql");
         return DriverManager.getConnection(url, user, password);
     }
 }
