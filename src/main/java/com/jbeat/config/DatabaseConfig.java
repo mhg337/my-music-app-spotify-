2a328ec5af427f5dd56525444035ce010ce8ce06
minhg <minhg337@gmail.com>
2026-06-08 21:58:41 +0900

    클라우드 DB

--- src/main/java/com/jbeat/config/DatabaseConfig.java
+++ src/main/java/com/jbeat/config/DatabaseConfig.java
@@ -5,18 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConfig {
-    private static final String URL = "jdbc:mysql://localhost:3306/jbeat?useSSL=false&serverTimezone=UTC&characterEncoding=UTF-8";
-
-    private static final String USER = "root";
-    private static final String PASSWORD = "mysql";
+    static String url = System.getenv("jdbc:postgresql://ep-weathered-darkness-aq4i3rey-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require");
+    static String user = System.getenv("neondb_owner");
+    static String password = System.getenv("npg_9zOsAYfCU1NX");
 
     public static Connection getConnection() throws SQLException {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL 드라이버를 찾을 수 없습니다.");
-            e.printStackTrace();
-        }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(url, user, password);
     }
 }
